dfb6683e9932fa6c6ed693a93e7fc176c0f3c9d4
Dave Morgan <dave@dmorg.org>
2025-03-17 00:07:17 -0600

    installed sass, svelte-preprocess and open-props

---
+++ src/lib/components/HomeLink.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let color = 'black';
+	export let shadow = 'white';
+</script>
+
+<a href="/" style="color: {color}; text-shadow: 0 1px 0 {shadow};">eduk8r.org</a>
+
+<style lang="scss">
+	a:link,
+	a:visited {
+		font-family: 'Rocksoldier', sans-serif;
+		font-weight: bold;
+		font-size: 4cqw;
+		letter-spacing: 0.25rem;
+		margin-top: -0.5vw;
+		padding: 1rem;
+		padding-block: 0;
+		position: absolute;
+		text-decoration: none;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [sveltePreprocess(), mdsvex()],
 
 	kit: {
 		adapter: adapter()
 	},
 
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
