c5c6e30f89f9d435eb0a19caaf3c218acda71a54
Dave Morgan <dave@dmorg.org>
2025-09-09 01:45:30 -0600

    belt lenght

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,9 +17,9 @@
 			<a href="/misc/calcs/fuelConverter">Metric/Imperial Fuel Converter</a>
 		</div>
 	</Card>
-	<Card header="Just for fun" theme="#bada55" secondary="#87a744">
+	<Card header="Geometry" theme="#BC8F8F" secondary="#8F5656">
 		<div class="flex">
-			<a href="/misc/wordle8r">Wordle8r</a>
+			<a href="/misc/nerdery">Belt-Length Calculation</a>
 		</div>
 	</Card>
 	<Card header="Head Scratchers" theme="Khaki" secondary="DarkKhaki">
@@ -27,6 +27,12 @@
 			<a href="/misc/montyHall">Monty Hall Simul8r</a>
 		</div>
 	</Card>
+	<Card header="Just for fun" theme="#bada55" secondary="#87a744">
+		<div class="flex">
+			<a href="/misc/wordle8r">Wordle8r</a>
+		</div>
+	</Card>
+
 	<Card header="Utilities" theme="CornflowerBlue" secondary="SteelBlue">
 		<div class="flex">
 			<a href="/utils/ccc">Color codes, shader and blender</a>
---
+++ src/routes/misc/nerdery/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header theme="#a00" />
+
+<div class="container">
+	<h3>Geometry Stuff</h3>
+	<div>
+		<p>
+			It was geometry in high school that turned mathematics from something that didn't interest me
+			into a lifelong passion. Nothing much here yet but stuff will creep in over time.
+		</p>
+		<br />
+		<p>
+			If the document is a .pdf file, it is best to download and view it in a laptop browser,
+			stepping through the presentation for best results. Scrolling through on a phone just isn't
+			the same!
+		</p>
+	</div>
+
+	<ul>
+		<li>
+			<a href="/nerdery/Nerdery.pdf">Belt length</a>
+			<a href="/nerdery/Nerdery.pdf" download>(Download .pdf)</a>
+		</li>
+	</ul>
+
+	<div class="feedback eighty">
+		Please send feedback and notification of typos or errors, etc., to
+		<a href="mailto:bugs.eduk8r@gmail.com?subject=Step by Step Feedback">bugs.eduk8r@gmail.com</a>
+	</div>
+</div>
+
+<style lang="scss">
+	.container {
+		align-items: center;
+		background-color: transparent;
+		// background-color: pink;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		margin-inline: auto;
+		width: 40rem;
+	}
+	h3,
+	h4 {
+		color: #a00;
+	}
+	a:link,
+	a:visited,
+	a:active {
+		color: #a00;
+
+		&:hover {
+			text-decoration: none;
+			text-shadow: 1px 1px 2px black;
+		}
+	}
+	ul {
+		list-style-type: none;
+		display: flex;
+		// background-color: gray;
+		padding: 0;
+		margin: 0;
+		margin-top: 3rem;
+	}
+	div,
+	ul,
+	li {
+		text-align: center;
+	}
+</style>
